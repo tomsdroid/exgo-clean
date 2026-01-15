1f17c051e78c0962a77e40438c5b0b85132994d9
Toms Droid <tomsdroid007@gmail.com>
2026-01-16 00:01:30 +0700

    first commit...

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+
+
+                FileUpload::make('image')
+                    ->image()
+                    ->avatar()
+                    ->imageEditor()
+                    ->circleCropper(),
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->required(),
+                TextInput::make('password')
+                    ->password()
+                    ->required(),
+            ]);
+    }
+}
---
+++ app/Http/Controllers/PromoController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Promo;
+use Illuminate\Http\Request;
+
+class PromoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Promo $promo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Promo $promo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Promo $promo)
+    {
+        //
+    }
+}
